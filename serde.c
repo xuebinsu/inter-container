6c75b0a27e4ee673064f43767d04b69b6547a9fd
Xuebin Su <xsu@pivotal.io>
2019-06-26 08:50:56 +0000

    Fix bugs in memcpy2()

--- serde.c
+++ serde.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <immintrin.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -5,17 +6,25 @@
 #include <string.h>
 #include <time.h>
 
+#define N (1 << 30)
+
+#define ALIGN 256
+
 void memcpy2(void* dest, const void* src, size_t size) {
-  for (size_t i = 0; i < size / 8; i++) {
-    __m256i tmp = _mm256_stream_load_si256(((__m256i*)src));
-    _mm256_store_si256(((__m256i*)dest), tmp);
+  size_t len = size / (ALIGN / 8);
+  for (size_t i = 0; i < len; i++) {
+    __m256i tmp = _mm256_stream_load_si256(((__m256i*)src) + i);
+    _mm256_store_si256(((__m256i*)dest) + i, tmp);
   }
   _mm_mfence();
 }
 
-#define N 1 << 30
-
-#define ALIGN 256
+void memcpy3(void* dest, const void* src, size_t size) {
+  for (size_t i = 0; i < size / sizeof(int); i++) {
+    int tmp = ((int*)src)[i];
+    ((int*)dest)[i] = tmp;
+  }
+}
 
 int main() {
   void* src = aligned_alloc(ALIGN, N);
@@ -23,19 +32,32 @@ int main() {
   printf("memset src = %d\n", ((char*)src)[0]);
   void* dest = aligned_alloc(ALIGN, N);
   memset(dest, 0, N);
+  for (int i = 0; i < N; i++) {
+    assert(((char*)dest)[i] == 0);
+  }
 
   clock_t begin = clock();
   memcpy2(dest, src, N);
   clock_t end = clock();
   double time_spent = (double)(end - begin);
-  printf("memcpy2 src = %d, dest = %d, time = %f\n", ((char*)src)[0],
-         ((char*)dest)[0], time_spent);
+  for (int i = 0; i < N; i++) {
+    assert(((char*)dest)[i] == 1);
+  }
+  printf("memcpy2 src = %d, dest = %d, time = %f\n", ((char*)src)[N - 1],
+         ((char*)dest)[N - 1], time_spent);
 
   begin = clock();
   memcpy(dest, src, N);
   end = clock();
   time_spent = (double)(end - begin);
-  printf("memcpy src = %d, dest = %d, time = %f\n", ((char*)src)[0],
-         ((char*)dest)[0], time_spent);
+  printf("memcpy src = %d, dest = %d, time = %f\n", ((char*)src)[N - 1],
+         ((char*)dest)[N - 1], time_spent);
+
+  begin = clock();
+  memcpy3(dest, src, N);
+  end = clock();
+  time_spent = (double)(end - begin);
+  printf("memcpy3 src = %d, dest = %d, time = %f\n", ((char*)src)[N - 1],
+         ((char*)dest)[N - 1], time_spent);
   return 0;
 }
